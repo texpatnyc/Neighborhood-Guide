743977fb9a5eda6ef143355c4841b8ee9010f201
Kyle Combs <combskyle@gmail.com>
2018-08-10 12:45:58 -0400

    added favicon

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -8,7 +8,6 @@ const router = express.Router();
 
 const jsonParser = bodyParser.json();
 
-// Post to register a new user
 router.post('/', jsonParser, (req, res) => {
   const requiredFields = ['username', 'password'];
   const missingField = requiredFields.find(field => !(field in req.body));
@@ -36,13 +35,6 @@ router.post('/', jsonParser, (req, res) => {
     });
   }
 
-  // If the username and password aren't trimmed we give an error.  Users might
-  // expect that these will work without trimming (i.e. they want the password
-  // "foobar ", including the space at the end).  We need to reject such values
-  // explicitly so the users know what's happening, rather than silently
-  // trimming them and expecting the user to understand.
-  // We'll silently trim the other fields, because they aren't credentials used
-  // to log in, so it's less of a problem.
   const explicityTrimmedFields = ['username', 'password'];
   const nonTrimmedField = explicityTrimmedFields.find(
     field => req.body[field].trim() !== req.body[field]
@@ -63,8 +55,6 @@ router.post('/', jsonParser, (req, res) => {
     },
     password: {
       min: 7,
-      // bcrypt truncates after 72 characters, so let's not give the illusion
-      // of security by storing extra (unused) info
       max: 72
     }
   };
@@ -93,8 +83,7 @@ router.post('/', jsonParser, (req, res) => {
   }
 
   let {username, password, firstName = '', lastName = '', hometown = ''} = req.body;
-  // Username and password come in pre-trimmed, otherwise we throw an error
-  // before this
+
   firstName = firstName.trim();
   lastName = lastName.trim();
 
