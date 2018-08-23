f40671b09372a7d6e9343d5982a1171fbc72004b
Kyle Combs <combskyle@gmail.com>
2018-08-23 16:29:29 -0700

    fixed CORS issue

--- public/index.js
+++ public/index.js
@@ -18,35 +18,26 @@ function activatePlacesSearch() {
     let autocomplete = new google.maps.places.Autocomplete(input, options);
     google.maps.event.addListener(autocomplete, 'place_changed', function() {
   		const selectedBusiness = autocomplete.getPlace();
-			placeId = selectedBusiness.place_id;
+		placeId = selectedBusiness.place_id;
     });
 };
 
-function autoFillForm(jsonObj, ref) {
-	document.getElementById('name').value = jsonObj.result.name;
-	document.getElementById('address').value = jsonObj.result.formatted_address;
-	document.getElementById('phone').value = jsonObj.result.formatted_phone_number;
-	document.getElementById('webUrl').value = jsonObj.result.website;
-	document.getElementById('photoLink').value = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=600&photoreference=${ref}&key=${googleApiKey}`;
-} 
-
+function autoFillForm(jsonObj) {
+	document.getElementById('name').value = jsonObj.name;
+	document.getElementById('address').value = jsonObj.formatted_address;
+	document.getElementById('phone').value = jsonObj.formatted_phone_number;
+	document.getElementById('webUrl').value = jsonObj.website;
+	document.getElementById('photoLink').value = jsonObj.photos[0].getUrl({'maxWidth': 600, 'maxHeight': 600});
+}
 
 function getPlaceDetails(query) {
-	const settings = {
-		url: 'https://maps.googleapis.com/maps/api/place/details/json',
-		data: {
-			placeid: query,
-			key: googleApiKey
-		},
-		dataType: 'json',
-		type: 'GET',
-		success: function(data) {
-			console.log(data);
-			let photoRef = data.result.photos[0].photo_reference;
-			autoFillForm(data, photoRef);
-		}
-	}
-	$.ajax(settings);
+	console.log(query);
+	const service = new google.maps.places.PlacesService(document.querySelector('body').appendChild(document.createElement('div')));
+	service.getDetails({
+    	placeId: query
+  	}, function (place, status) {
+	autoFillForm(place);
+  });
 }
 
 
