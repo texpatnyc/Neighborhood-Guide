cfeabd740e4db2e65f754959e1316048cc39e91c
Kyle Combs <combskyle@gmail.com>
2018-08-29 11:27:08 -0400

    changed test db

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ exports.DATABASE_URL =
 	process.env.DATABASE_URL || 
 	global.DATABASE_URL ||
 	'mongodb://neighborhoodguide:26stmarksplace@ds127362.mlab.com:27362/neighborhood-guide';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-guide-app';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://neighborhoodguide:26stmarksplace@ds137862.mlab.com:37862/test-neighborhood-guide';
 exports.PORT = process.env.PORT || 8080;
